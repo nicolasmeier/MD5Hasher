63a512cc54e7fd01ab9c8387ae2b76b5d23c3e1b
Nicolas Meier <nicolas.meier97@gmail.com>
2016-04-28 19:33:34 +0200

    First Build

---
+++ MD5Hasher/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace MD5Hasher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            DialogResult result = dialog.ShowDialog();
+            textBlock.Text = dialog.FileName;
+            textBox.Text = ByteArrayToString(GetMD5Hash(dialog.FileName));
+        }
+
+        private byte[] GetMD5Hash(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return md5.ComputeHash(stream);
+                }
+            }
+        }
+
+
+        private string ByteArrayToString(byte[] buffer)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                sb.Append(buffer[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+    }
+}
